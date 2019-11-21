2873d0e9eb769e538ff0ba30e6907eba92785e00
Fibonacci998 <30697927+tuansu2021998@users.noreply.github.com>
2019-11-21 08:27:47 +0700

    update exchange

--- smartcontract/Exchange.sol
+++ smartcontract/Exchange.sol
@@ -1,5 +1,24 @@
 pragma solidity ^0.4.17;
-import './Reserve.sol';
+// import './Reserve.sol';
+interface ERC20 {
+    function totalSupply() public constant returns (uint);
+    function balanceOf(address tokenOwner) public constant returns (uint balance);
+    function allowance(address tokenOwner, address spender) public constant returns (uint remaining);
+    function transfer(address to, uint tokens) public returns (bool success);
+    function approve(address spender, uint tokens) public returns (bool success);
+    function transferFrom(address from, address to, uint tokens) public returns (bool success);
+    event Transfer(address indexed from, address indexed to, uint tokens);
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+}
+interface Reserve{
+    function withdraw(address tokenAddress, uint amount) public;
+    function getExchangeRate(bool isBuy) public view returns(uint);
+    function setExchangeRates(uint buyRate, uint sellRate) public;
+    function setTradeFlag(bool value) public;
+    function exchange(bool _isBuy, uint amount) payable public returns(uint);
+    function getBalance()public view returns(uint);
+    function getBalanceToken() public view returns(uint);
+}
 contract Exchange{
     //address of owner
     address owner;
@@ -13,58 +32,70 @@ contract Exchange{
     }
     function addReserve(address reserveAddress, address tokenAddress ,bool isAdd) public{
         if (isAdd){
-            Reserve temp = new Reserve(0,0,true,tokenAddress,1,1);
-            listReserves[reserveAddress]= temp;
+            Reserve temp = Reserve(reserveAddress);
+            listReserves[tokenAddress]= temp;
         }else{
-            delete(listReserves[reserveAddress]);
+            delete(listReserves[tokenAddress]);
         }
     }
-    function getExchangeRate(address srcToken, address destToken, uint amount) public returns(uint){
+    function setExchangeRate(address tokenAddress,uint buyRate, uint sellRate) public onlyOwner{
+        listReserves[tokenAddress].setExchangeRates(buyRate, sellRate);
+    }
+    function getExchangeRate(address srcToken, address destToken, uint amount) public view returns(uint){
         if (destToken == srcToken){
             return 1;
         }
-        if (destToken != addressEth && srcToken!= addressEth && destToken != addressEth && srcToken != addressEth){
-            Reserve reserve1 = listReserves[srcToken];
-            uint rate1 = reserve1.getExchangeRate(false, amount);
-            reserve1 = listReserves[destToken];
-            rate1 = reserve1.getExchangeRate(true, rate1);
-            return rate;
+        if (destToken != addressEth && srcToken!= addressEth){
+            Reserve reserveDest = listReserves[destToken];
+            Reserve reserveSrc = listReserves[srcToken];
+            
+            uint rateSellToSrc = reserveSrc.getExchangeRate(false);
+            uint rateBuyFromDest = reserveDest.getExchangeRate(true);
+            uint rate1 = (amount*(10**18)/rateSellToSrc)*rateBuyFromDest;
+            return rate1;
         }
         if (srcToken == addressEth){
-            //swap from eth to custom token
+            //swap from eth to custom token ~ buy token
             Reserve reserve =  listReserves[destToken];
-            uint rate = reserve.getExchangeRate(true, amount);
+            uint rate = amount*(10**18)/reserve.getExchangeRate(true);
             return rate;
         }
         if (destToken == addressEth){
             //swap from custom token to eth
             reserve =  listReserves[srcToken];
-            rate = reserve.getExchangeRate(false, amount);
+            rate = amount * (10**18)*reserve.getExchangeRate(false);
             return rate;
         }
     }
-    function exchangeTokens(address srcToken, address destToken, uint amount) public{
+    function exchangeTokens(address srcToken, address destToken, uint amount) public payable{
         if (destToken == srcToken){
             return;
         }
-        if (destToken != addressEth && srcToken!= addressEth && destToken != addressEth && srcToken != addressEth){
+        if (destToken != addressEth && srcToken!= addressEth){
             Reserve reserve1 = listReserves[srcToken];
-            reserve1.exchange(false, amount);
+            uint amountEth = reserve1.exchange(false, amount);
             reserve1 = listReserves[destToken];
-            reserve1.exchange(true, amount);
-            return rate;
+            reserve1.exchange(true, amountEth);
+            ERC20(destToken).transfer(msg.sender, amount);
         }
         if (srcToken == addressEth){
             //swap from eth to custom token
+            require((msg.value) == (amount*(10**18)));
             Reserve reserve =  listReserves[destToken];
-            uint rate = reserve.exchange(true, amount);
-            return rate;
+            reserve.exchange(true, amount);
+            ERC20(destToken).transfer(msg.sender, amount);
         }
         if (destToken == addressEth){
             //swap from custom token to eth
             reserve =  listReserves[srcToken];
-            rate = reserve.exchange(false, amount);
-            return rate;
+            reserve.exchange(false, amount);
+            msg.sender.transfer(amount*(10**18));  
         }
     }
+    
+    modifier onlyOwner(){
+        require(msg.sender == owner);
+        _;
+    }
+    event Transfer(address indexed from, address indexed to, uint tokens);
 }
